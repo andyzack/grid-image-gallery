101cf322581521121f1259ae2e46e4db3531457e
Andrew Zacharias <andrewz@evolution7.com.au>
2021-11-04 16:33:41 +1100

    Add vue project starter

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="{ name: 'EventDetails', params: { id: event.id } }">
+    <div class="event-card">
+      <span>@{{ event.time }} {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
